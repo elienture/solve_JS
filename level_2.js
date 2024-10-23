4f80fcb63607139b63dd55fd660a0b5f9f6c6b3d
Liza Nauman <naumanes@mail.ru>
2024-10-23 20:32:39 +0300

    new solutions

--- level_2.js
+++ level_2.js
@@ -41,3 +41,81 @@ let NumbersArray = [-1, -2, -3, 3, 2, 16, -5]
 const positiveArray = NumbersArray.filter(num => num > 0)
 
 console.log(positiveArray)
+
+// Даны два слова. Проверьте, что последняя буква первого слова совпадает с первой буквой второго слова.
+
+function letterMatch (firstWord, secondWord) {
+    let lastLetter = firstWord[firstWord.length - 1];
+    let secondLetter = secondWord[0];
+
+    if (lastLetter == secondLetter) {
+        console.log('true')
+    } else {
+        console.log('false')
+    }
+}
+
+letterMatch('front', 'back')
+
+// Дана дата в следующем формате: '2025-12-31' Преобразуйте эту дату в следующий объект: { year: '2025', month: '12', day: '31', }
+
+function parseDate (dateString) {
+    const [year, month, day] = dateString.split('-');
+    return {
+        year: year,
+        month: month,
+        day: day
+    }
+}
+
+console.log(parseDate('2025-12-31'))
+
+// Даны числа, разделенные запятыми: '12,34,56' Найдите сумму этих чисел.
+
+let numbersString = '12,34,56';
+let numSeperate = numbersString.split(','); 
+let sum = numSeperate.reduce((acc, val) => acc + parseInt(val), 0); 
+
+console.log(sum); 
+
+// Дан объект с ключами и значениями. Запишите в первый массив ключи объекта, а во второй - значения.
+
+const obj = {
+    name: 'L',
+    age: 100,
+    sign: 'scorpio'
+};
+
+const keysArray = Object.keys(obj);
+const valuesArray = Object.values(obj);
+
+console.log(keysArray)
+console.log(valuesArray)
+
+// Дана некоторая строка с буквами и цифрами. Получите позицию первой цифры в этой строке.
+
+function digitFind(string) {
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] >= 0 && string[i] <= 9) { 
+            return i; // Возвращаем индекс первой цифры
+        }
+    }
+}
+
+console.log(digitFind('a1b2c3d4'));
+
+// Дана некоторая строка: 'abcde' Переведите в верхний регистр все нечетные буквы этой строки.
+//  В нашем случае должно получится следующее: 'AbCdE'
+
+function oddLetters(string) {
+    return string
+        .split('').map((letter, index) => {
+            if (index % 2 !== 0) {
+                return letter.toUpperCase(); 
+            }
+            return letter; 
+        }).join(''); 
+}
+
+
+console.log(oddLetters('abcde'))
